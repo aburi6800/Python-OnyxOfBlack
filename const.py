4d7d23699ad5083588c44c15ca9149d247c6bc9e
aburi-dev <aburi6800@gmail.com>
2020-05-31 10:26:36 +0900

    Test module

---
+++ const.py
@@ -0,0 +1,15 @@
+"""
+Constant types in Python.
+"""
+
+class _const:
+    class ConstError(TypeError):
+        pass
+
+    def __setattr__(self, name, value):
+        if name in self.__dict__:
+            raise self.ConstError("Can't rebind const (%s)" % name)
+        self.__dict__[name] = value
+
+import sys
+sys.modules[__name__]=_const()
